af5f0f6eee2241619f96b5bd8f6bf8fa9b1eff39
Chris Matthieu <chris@matthieu.us>
2017-03-08 20:48:14 -0700

    added watson speech

--- app.js
+++ app.js
@@ -36,141 +36,72 @@ app.get('/audio/new', function(req, res){
 app.post('/audio/new', function(req, res) {
   console.log(req.files);
   let sampleFile = req.files.audio;
-  sampleFile.mv('./public/audio/temp.raw');
+  // sampleFile.mv('./public/audio/temp.raw');
+  sampleFile.mv('./public/audio/temp.wav');
 
-  // var opts = {
-  //   filetype: 'mp3',
-  //   key: key
-  // };
-  // request
-  //   // .get(req.files.audio.path)
-  //   .get(req.files.audio)
-  //   .pipe(speech(opts, function (err, results) {
-  //     // handle the results
-  //     console.log(results);
-  //   }));
-
-
-
-
-  // var opts = {
-  //   file: './audio/allison.flac',
-  //   keyFilename: './keyfile.json'
-  // };
-  //
-  // speech(opts, function (err, results) {
-  //   console.log(results);
-  //   res.send(results);
-  //   // [{result: [{alternative: [{transcript: '...'}]}]}]
-  // });
-
-
-
-  // var tts = spawn('stenographer', ['-t', 'wav']);
-  // filed(req.files.audio.path).pipe(tts.stdin);
-
-  // // processAudio(req.files.audio.path, function(result){
-  //   // console.log(result);
-  //   // res.send('Uploaded ' + req.files.audio.name + ' to ' + req.files.audio.path);
-  //   tts.stdout.pipe(res);
-  // // })
-
-
-
-  // var googspeech = require('@google-cloud/speech')({
-  //   projectId: 'dictation-159501'
-  //   // keyFilename: require('./keyfile.json'),
-  //   // credentials: require('./keyfile.json')
-  // });
-  // googspeech.recognize('./audio/allison.flac', {
-  //   encoding: 'FLAC',
-  //   sampleRate: 16000,
-  //   languageCode: 'en-US'
-  // }, function(err, transcript) {
-  //   console.log(transcript);
-  // });
-
-
-
-
-  // // [START speech_async_recognize]
+  // Google Speech API
   // // Imports the Google Cloud client library
   // const Speech = require('@google-cloud/speech');
   //
+  // // Your Google Cloud Platform project ID
+  // const projectId = 'dictation-159501';
+  //
   // // Instantiates a client
-  // const speech = Speech({
-  //   projectId: 'dictation-159501',
-  //   keyFilename: '/Users/topher/Projects/Misc/dictation-api/keyfile.json',
-  //   languageCode: 'en-US',
-  //   credentials: require('./keyfile.json')
+  // const speechClient = Speech({
+  //   projectId: projectId
   // });
   //
-  // // The path to the local file on which to perform speech recognition, e.g. /path/to/audio.raw
-  // // const filename = '/path/to/audio.raw';
+  // // The name of the audio file to transcribe
+  // // const fileName = './public/audio/audio.raw';
+  // // const fileName = './public/audio/Audio.mp3';
+  // const fileName = './public/audio/temp.raw';
   //
-  // // The encoding of the audio file, e.g. 'LINEAR16'
-  // // const encoding = 'LINEAR16';
-  //
-  // // The sample rate of the audio file, e.g. 16000
-  // // const sampleRate = 16000;
-  //
-  // const request = {
-  //   encoding: 'FLAC',
-  //   sampleRate: 16000,
-  //   languageCode: 'en-US'
+  // // The audio file's encoding and sample rate
+  // const options = {
+  //   encoding: 'LINEAR16',
+  //   // encoding: 'FLAC',
+  //   sampleRate: 16000
   // };
   //
-  // // Detects speech in the audio file. This creates a recognition job that you
-  // // can wait for now, or get its result later.
-  // speech.startRecognition('/Users/topher/Projects/Misc/dictation-api/audio/allison.flac', request)
+  // // Detects speech in the audio file
+  // speechClient.recognize(fileName, options)
   //   .then((results) => {
-  //     const operation = results[0];
-  //     // Get a Promise represention of the final result of the job
-  //     return operation.promise();
-  //   })
-  //   .then((transcription) => {
+  //     const transcription = results[0];
   //     console.log(`Transcription: ${transcription}`);
+  //     res.send(`Transcription: ${transcription}`);
   //   });
-  // // [END speech_async_recognize]
 
 
 
 
+  // IBM Watson / BlueMix
+  var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+  var fs = require('fs');
 
-  // [START speech_quickstart]
-  // Imports the Google Cloud client library
-  const Speech = require('@google-cloud/speech');
-
-  // Your Google Cloud Platform project ID
-  const projectId = 'dictation-159501';
-
-  // Instantiates a client
-  const speechClient = Speech({
-    projectId: projectId
+  var speech_to_text = new SpeechToTextV1({
+    username: 'watson-speech-to-text-username',
+    password: 'watson-speech-to-text-password'
   });
 
-  // The name of the audio file to transcribe
-  // const fileName = './public/audio/audio.raw';
-  // const fileName = './public/audio/Audio.mp3';
-  const fileName = './public/audio/temp.raw';
-
-  // The audio file's encoding and sample rate
-  const options = {
-    encoding: 'LINEAR16',
-    // encoding: 'FLAC',
-    sampleRate: 16000
+  var params = {
+    // From file
+    audio: fs.createReadStream('./public/audio/temp.wav'),
+    content_type: 'audio/l16; rate=44100'
   };
 
-  // Detects speech in the audio file
-  speechClient.recognize(fileName, options)
-    .then((results) => {
-      const transcription = results[0];
-      console.log(`Transcription: ${transcription}`);
-      res.send(`Transcription: ${transcription}`);
-    });
-  // [END speech_quickstart]
+  speech_to_text.recognize(params, function(err, result) {
+    if (err)
+      console.log(err);
+    else
+      // console.log(JSON.stringify(res, null, 2));
+      console.log(`Transcription: ${JSON.stringify(result, null, 2)}`);
+      res.send(`Transcription: ${JSON.stringify(result, null, 2)}`);
+  });
 
-
+  // // or streaming
+  // fs.createReadStream('./resources/speech.wav')
+  //   .pipe(speech_to_text.createRecognizeStream({ content_type: 'audio/l16; rate=44100' }))
+  //   .pipe(fs.createWriteStream('./transcription.txt'));
 
 });
 
